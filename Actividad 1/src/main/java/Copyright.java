6eca9b3d81615fb0b4bfd084da3e55fdd63efffa
Celia Romero <celiaromerodiaz22@gmail.com>
2020-04-02 17:02:17 +0200

    Añadida Actividad 1

---
+++ Actividad 1/src/main/java/Copyright.java
@@ -0,0 +1,14 @@
+public class Copyright {
+
+    public String getText()
+    {
+        return "Copyright (C) 2020 Celia Romero Díaz - All Rights Reserved\n" +
+                "        You may use, distribute and modify this code under the\n" +
+                "        terms of the XYZ license, which unfortunately won't be\n" +
+                "        written for another century.\n" +
+                "        \n" +
+                "        You should have received a copy of the XYZ license with\n" +
+                "        this file. If not, please visit : http://www.github.com/";
+    }
+
+}
